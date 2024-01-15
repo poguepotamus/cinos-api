bf355a5e2e94ebb668cf1c14e157c375f22e0263
unknown <matthewpogue606@gmail.com>
2024-01-15 15:31:14 -0600

    Added code that compelted the first assignment

---
+++ Drink.py
@@ -0,0 +1,39 @@
+class Drink:
+
+    bases = ['water', 'sbrite', 'pokeacola', 'Mr. Salt', 'hill fog', 'leaf wine']
+    flavors = ['lemon', 'cherry', 'strawberry', 'mint', 'blueberry', 'lime']
+
+    def __init__(self, base, flavors:set=None, total:float=1):
+        # If our base isn't provided, we raise an exception
+        if base not in Drink.bases:
+            raise ValueError('Invalid drink base')
+        self._base = base
+
+        # Setting our flavors. Defaults to an empty set
+        self._flavors = flavors or {}
+
+        self._total = total
+
+    def get_flavors(self):
+        return self._flavors
+
+    def get_base(self):
+        return self._base
+
+    def get_total(self):
+        return self._total
+
+    def get_num_flavors(self):
+        return len(self._flavors)
+
+    def set_flavors(self, flavors:set):
+        if type(flavors) not in [set, list]:
+            raise ValueError('Provided flavors must be set or list')
+        if not all(flavor in Drink.flavors for flavor in flavors):
+            raise ValueError(f'Invalid flavor provided in {flavors}')
+        self._flavors = set(flavors)
+
+    def add_flavor(self, flavor:str, amount:float):
+        if flavor not in Drink.flavors:
+            raise ValueError('Invalid flavor')
+        self._flavors[flavor] = amount
---
+++ Order.py
@@ -0,0 +1,22 @@
+from Drink import Drink
+
+class Order:
+    def __init__(self):
+        self._items = []
+
+    def get_items(self):
+        return self._items
+
+    def get_num_items(self):
+        return len(self._items)
+
+    def get_total(self):
+        return sum(item.get_total() for item in self._items)
+
+    def add_item(self, item:Drink):
+        self._items.append(item)
+        return self
+
+    def remove_item(self, index:int):
+        self._items.pop(index)
+        return self
